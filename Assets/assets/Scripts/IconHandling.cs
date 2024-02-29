9db4a97e0c18bab3b58f9b47525a52d1e7ebb64c
T-cube2512 <srivenk03@gmail.com>
2024-02-29 15:42:18 +0530

    virus started

--- Assets/assets/Scripts/IconHandling.cs
+++ Assets/assets/Scripts/IconHandling.cs
@@ -4,63 +4,72 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-
-
-public class IconHandling : MonoBehaviour ,IPointerEnterHandler, IPointerExitHandler , IPointerClickHandler
+public class IconHandling : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-
-    public Image img;    
-
+    public Image img;
     public GameObject desktop;
-
-    public Color noclickcol = new Color(1,1,1,0);
-    public Color hovcol = new Color(1,1,1,0.3f);
-    public Color clickcol = new Color(1,1,1,0.7f);
-
-    private bool clicked;
-    private bool doubleclicked;
+    public Sprite virusbg;
+    public Color noclickcol = new Color(1, 1, 1, 0);
+    public Color hovcol = new Color(1, 1, 1, 0.3f);
+    public Color clickcol = new Color(1, 1, 1, 0.7f);
 
     public bool isVirus;
     public bool isTaskbarIcon;
 
-
-
+    private bool clicked;
+    public bool doubleClicked;
+    private float clickTimeThreshold = 0.3f; // Adjust this threshold as needed
+    private float lastClickTime;
 
     void Start()
     {
-        img.color =  noclickcol;
+        img.color = noclickcol;
         clicked = false;
+        doubleClicked = false;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         changeCol();
-        Debug.Log("hover enter");
-
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        backtonormal();
-        Debug.Log("hover stop");
+        backToNormal();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        clickchange();
-        Debug.Log("clicked");
+        float timeSinceLastClick = Time.time - lastClickTime;
+        if (timeSinceLastClick <= clickTimeThreshold)
+        {
+            doubleClicked = true;
+            // Perform double click action here
+            if(doubleClicked && isVirus)
+            {
+                desktop.GetComponent<Image>().sprite = virusbg;
+            }
+        }
+        else
+        {
+            doubleClicked = false;
+            lastClickTime = Time.time;
+            clickChange();
+        }
     }
 
-
-    public void changeCol() {
+    public void changeCol()
+    {
         img.color = hovcol;
     }
 
-    public void backtonormal() {
+    public void backToNormal()
+    {
         img.color = noclickcol;
     }
 
-    public void clickchange(){
+    public void clickChange()
+    {
         if (!isTaskbarIcon)
         {
             if (img.color == hovcol || img.color == noclickcol)
@@ -76,11 +85,9 @@ public class IconHandling : MonoBehaviour ,IPointerEnterHandler, IPointerExitHan
         }
     }
 
-    private void Update() {
-        if(clicked) img.color = clickcol;
-        
-
-
+    private void Update()
+    {
+        if (clicked)
+            img.color = clickcol;
     }
-    
 }
